1d5cdb0c358d9f3a96c7b46b5cfef2603508942e
Roman Pavlyuchenko <romanpavlyuchenko@inbox.ru>
2023-04-20 15:50:31 +0300

    user model created

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
+from auth.base_config import auth_backend, fastapi_users
+from auth.schemas import UserCreate, UserRead
+
 
 app = FastAPI(
     title="Recipe App"
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
 
-@app.get("/")
-def get_hello():
-    return "Hello world!"
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
